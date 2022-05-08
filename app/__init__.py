a5b1c32e2588eced473823ab2fa8596407aaaac1
Edu58 <edumuriithi58@gmail.com>
2022-05-08 09:39:33 +0300

    adds auth blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,25 @@
 from flask import Flask
+from flask_login import LoginManager
+from flask_bootstrap import Bootstrap
+
+login = LoginManager()
+login.login_view = "auth.login"
+login.session_protection = "strong"
+login.login_message_category = "warning"
+bootstrap = Bootstrap()
 
 
 def create_app():
     app = Flask(__name__)
 
+    login.init_app(app)
+
+    bootstrap.init_app(app)
+
     from app.main import main
     app.register_blueprint(main)
+
+    from app.auth import auth
+    app.register_blueprint(auth, url_prefix="auth/")
 
     return app
