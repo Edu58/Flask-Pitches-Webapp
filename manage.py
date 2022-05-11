5d1e7d7c46a84e3a719990e37012016803aad1fd
Edu58 <edumuriithi58@gmail.com>
2022-05-11 11:47:37 +0300

    removes shell script in manage.py

--- manage.py
+++ manage.py
@@ -1,24 +1,8 @@
 from app import create_app
 from flask_script import Manager
 
-# from app.models import Users, Pitches, Categories, Comments, Reactions
-
 app = create_app('production')
 manager = Manager(app)
-# manager.add_command('server', Server)
-
-# @manager.shell
-# def make_shell_context():
-#     return dict(app=app, db=db, users=Users, pitches=Pitches, categories=Categories, comments=Comments,
-#                 reactions=Reactions)
-#
-#
-# @manager.command
-# def test():
-#     import unittest
-#     tests = unittest.TestLoader().discover('test')
-#     unittest.TextTestRunner(verbosity=2).run(tests)
-
 
 if __name__ == '__main__':
     manager.run()
