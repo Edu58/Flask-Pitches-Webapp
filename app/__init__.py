147af9ade810e102600796e3334c310603c2472f
Edu58 <edumuriithi58@gmail.com>
2022-05-11 16:13:46 +0300

    application factory refractoring

--- app/__init__.py
+++ app/__init__.py
@@ -6,10 +6,9 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_moment import Moment
 from flask_uploads import IMAGES, UploadSet, configure_uploads
 
-app = None
 # from flask_mail import Mail
 
-db = SQLAlchemy(app)
+db = SQLAlchemy()
 bootstrap = Bootstrap5()
 login_manager = LoginManager()
 moment = Moment()
@@ -20,7 +19,6 @@ photos = UploadSet('photos', IMAGES)
 
 
 def create_app(configuration):
-    global app
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_object(config_options[configuration])
 
