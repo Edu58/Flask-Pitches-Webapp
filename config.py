79fa02728f0969ffd0eb19deca58044a5df0eb88
Edu58 <edumuriithi58@gmail.com>
2022-05-12 10:49:47 +0300

    recreates migrations

--- config.py
+++ config.py
@@ -2,8 +2,6 @@ import os
 
 
 class Config:
-    SECRET_KEY = "dskjyf5667UUGUvbaUGAUBUGYT8YGJB89Y8898989gbU8978t789G789T789"
-    SQLALCHEMY_DATABASE_URI = "postgresql+psycopg2://ed:ed@localhost/pitches"
     UPLOADED_PHOTOS_DEST = 'app/static/photos'
 
 
@@ -13,11 +11,11 @@ class DevConfig(Config):
 
 class ProdConfig(Config):
     DEBUG = False
-    # uri = os.getenv("DATABASE_URL")  # or other relevant config var
-    # if uri.startswith("postgres://"):
-    #     uri = uri.replace("postgres://", "postgresql://", 1)
-    # # rest of connection code using the connection string `uri`
-    # SQLALCHEMY_DATABASE_URI = uri
+    uri = os.getenv("DATABASE_URL")  # or other relevant config var
+    if uri.startswith("postgres://"):
+        uri = uri.replace("postgres://", "postgresql://", 1)
+    # rest of connection code using the connection string `uri`
+    SQLALCHEMY_DATABASE_URI = uri
 
 
 config_options = {
